06e4168b09fd0d89e8b2ba5738e577342994de25
hxrry27 <131454517+hxrry27@users.noreply.github.com>
2026-04-09 01:20:17 +0100

    installed spell ui bits, tested out shadcn versions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Signature } from "@/components/signature";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">test
-      <div>
+    <div className="min-h-svh flex flex-col justify-center px-6 gap-4 max-w-md mx-auto">
+      <div className="flex flex-col gap-1.5">
+        <h1>harry</h1>
+        <p>some <span className="font-bold">bold</span> text about me because i am <span className="font-medium">harry</span></p>
+        <Signature className="self-end" text="harry" fontSize={22} color="#4fb6d6" inView={false} />
       </div>
     </div>
   );
